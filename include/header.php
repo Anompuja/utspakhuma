30a99e54d2d70f740dd42220067d87c3693036f7
Anompuja <anomtugus@gmail.com>
2025-01-07 12:39:30 +0800

    up include biar nanti html aku ganti ke php

---
+++ include/header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Xbox Gaming Layout with Slider</title>
+    <link rel="stylesheet" href="assets/style.css" />
+  
+  <body>
+    <nav>
+      <div class="logo">Undiknas Hero</div>
+      <ul class="nav-links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="specialoffer.html">Special Offers</a></li>
+        <li><a href="trending.html">Trending</a></li>
+        <li><a href="newrelease.html">New Releases</a></li>
+      </ul>
+    </nav>
+  </head>
