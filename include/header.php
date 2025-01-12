62b2107d32547981a009d662d01c3a34bd3aa80f
dazzdizzduzz <dhaisalfarysy@gmail.com>
2025-01-12 21:29:39 +0800

    header fix nih cuy

--- include/header.php
+++ include/header.php
@@ -2,31 +2,33 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Xbox Gaming Layout with Slider</title>
-    <link rel="stylesheet" href="assets/style.css" />
+    <link rel="stylesheet" href="header.css" />
   
   <body>
     <nav>
         <div class="logo">
-            <img src="img/UGC.png" alt="logo" style="width: 150; padding-left: 30px;">
-        </div>
-        <ul class="nav-links" style="margin-right: 30px; display: flex; font-weight: bold;">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="specialoffer.html">Special Offers</a></li>
-            <li><a href="trending.html">Trending</a></li>
-         <li><a href="newrelease.html">New Releases</a></li>
-        </ul>
-        <div class="icon-container">
-            <div class="cart-icon">
-                <a href="checkout.html">
-                    <img src="img/cart.png" alt="Cart" class="cart-image" style="width: 40;">
-                    <span class="cart-count" onclick="location.href='checkout.html'"><span id="cart-count">0</span></style></span>
-                </a>
+            <div>
+                <img src="img/UGC.png" alt="logo" style="width: 150; padding-left: 30px;">
             </div>
         </div>
+            <div>
+                <ul class="nav-links">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="specialoffer.html">Special Offers</a></li>
+                    <li><a href="trending.html">Trending</a></li>
+                    <li><a href="newrelease.html">New Releases</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="icon-container">
+            <div class="cart-icon">
+                <a href="checkout.html"><img src="img/cart.png" alt="Cart" class="cart-image" style="width: 40;">
+                <span class="cart-count" onclick="location.href='checkout.html'"><span id="cart-count">0</span></style></span>
+            </div>
              <div class="profile">
-            <a href="login.html">
-                <img src="img/Profile.png" alt="Profile Icon" style="cursor: pointer; width: 44;">
-            </a>  
+                <a href="login.html">
+                    <img src="img/Profile.png" alt="Profile Icon" style="cursor: pointer; width: 44;">
+                </a>  
             </div>
         </div>
     </nav>
