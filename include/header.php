5c011e70e8a897b203d5cabab8d4ef91e5d406ac
dazzdizzduzz <dhaisalfarysy@gmail.com>
2025-01-12 18:10:13 +0800

    ini logo sama header

--- include/header.php
+++ include/header.php
@@ -6,15 +6,20 @@
   
   <body>
     <nav>
-      <div class="logo">
-        <img src="img/UGC.png" alt="">
-        
-      </div>
-      <ul class="nav-links">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="specialoffer.html">Special Offers</a></li>
-        <li><a href="trending.html">Trending</a></li>
-        <li><a href="newrelease.html">New Releases</a></li>
-      </ul>
+        <div class="logo">
+            <img src="img/UGC.png" alt="logo" style="width: 150; padding-left: 30px;">
+        </div>
+        <ul class="nav-links" style="margin-right: 30px; display: flex; font-weight: bold;">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="specialoffer.html">Special Offers</a></li>
+            <li><a href="trending.html">Trending</a></li>
+         <li><a href="newrelease.html">New Releases</a></li>
+        </ul>
+        <div class="profile">
+            <a href="login.html">
+                <img src="img/Profile.png" alt="Profile Icon" style="cursor: pointer; width: 44;">
+            </a>
+            
+        </div>
     </nav>
   </head>
