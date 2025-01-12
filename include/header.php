761eae1e05c6c51d531b9f70385ed6d5735d33a3
Anompuja <anomtugus@gmail.com>
2025-01-12 21:35:42 +0800

    menambahkan header baru

--- include/header.php
+++ include/header.php
@@ -1,23 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>New Releases</title>
-    <link rel="stylesheet" href="new-releases.css" />
+    <title>Xbox Gaming Layout with Slider</title>
+    <link rel="stylesheet" href="header.css" />
 
+<body>
     <nav>
-        <div class="logo"><img src="" alt=""></div>
-        <ul class="nav-links">
-            <li><a href="/utspakhuma/index.php">Home</a></li>
-            <li><a href="/utspakhuma/specialoffer.php">Special Offers</a></li>
-            <li><a href="/utspakhuma/trending.php">Trending</a></li>
-            <li><a href="/utspakhuma/newrelease.php">New Releases</a></li>
-        </ul>
-        <button class="cart-button" onclick="location.href='/utspakhuma/checkout.php'">Cart (<span
-                id="cart-count">0</span>)</button>
+        <div class="logo">
+            <div>
+                <img src="img/UGC.png" alt="logo" style="width: 150; padding-left: 30px;">
+            </div>
+        </div>
+        <div>
+            <ul class="nav-links">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="specialoffer.html">Special Offers</a></li>
+                <li><a href="trending.html">Trending</a></li>
+                <li><a href="newrelease.html">New Releases</a></li>
+            </ul>
+        </div>
+        </div>
+        <div class="icon-container">
+            <div class="cart-icon">
+                <a href="checkout.html"><img src="img/cart.png" alt="Cart" class="cart-image" style="width: 40;">
+                    <span class="cart-count" onclick="location.href='checkout.html'"><span id="cart-count">0</span>
+                        </style></span>
+            </div>
+            <div class="profile">
+                <a href="login.html">
+                    <img src="img/Profile.png" alt="Profile Icon" style="cursor: pointer; width: 44;">
+                </a>
+            </div>
+        </div>
     </nav>
-</head>
-
-</html>
+    </head>
